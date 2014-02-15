dfc8dbbf72a89336a8dd1d24c5dca9eb9edca151
nealcpeters <nealcpeters@gmail.com>
2014-02-15 15:14:29 -0600

    refactored fields in congress members table

--- db/migrate/20140215142515_create_congress_members.rb
+++ db/migrate/20140215142515_create_congress_members.rb
@@ -5,27 +5,15 @@ class CreateCongressMembers < ActiveRecord::Migration
     create_table :congress_members do |t|
       t.string  :title
       t.string  :firstname
-      t.string  :middlename
       t.string  :lastname
-      t.string  :name_suffix
-      t.string  :nickname
       t.string  :party
       t.string  :state
-      t.integer :district
       t.integer :in_office
       t.string  :gender
       t.string  :phone
-      t.fax     :fax
       t.string  :website
-      t.string  :webform
       t.string  :congress_office
-      t.string  :bioguide_id
-      t.string  :votesmart_id
-      t.string  :fec_id
-      t.string  :govtrack_id
-      t.string  :crp_id
       t.string  :twitter_id
-      t.string  :congresspedia_url
       t.string  :youtube_url
       t.string  :facebook_id
       t.string  :official_rss
