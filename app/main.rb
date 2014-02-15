7a8114bed7169d4d0613261c336d919a00de332d
nealcpeters <nealcpeters@gmail.com>
2014-02-15 16:41:24 -0600

    structured wishful method calls in app main script

---
+++ app/main.rb
@@ -0,0 +1,12 @@
+require_relative 'db/config'
+require_relative 'app/models/legislator'
+
+puts Legislator.list_by_state("TN")
+
+puts Legislator.list_by_gender("F")
+
+puts Legislator.list_active
+
+puts Legislator.total
+
+# active record method calls go here.
